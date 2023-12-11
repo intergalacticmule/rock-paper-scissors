3efddc9f04347a5daa4cbc9abab154bf7733ec52
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-11 17:10:39 +0200

    Add getPlayerChoice() functionality

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function getComupterChoice() {
   );
   return computerChoice;
 }
+
+function checkPlayerChoiceValidity(choice) {
+  if (choices.includes(String(choice.toLowerCase()))) {
+    return true;
+  }
+  return false;
+}
+
+function getPlayerChoice() {
+  let playerChoice;
+  while (
+    !checkPlayerChoiceValidity(
+      (playerChoice = prompt("Please enter your choice."))
+    )
+  ) {
+    alert(
+      'Your choice can only be "Rock", "Paper", or "Scissors". Letter case does not matter.'
+    );
+  }
+  console.log(
+    `Player choice: ${
+      playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
+    }`
+  );
+  return playerChoice.toLowerCase();
+}
